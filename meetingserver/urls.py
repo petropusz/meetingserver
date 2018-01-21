655d585c352bd53993e08948eb97acd5f8d5654e
Piotr Pusz <petropusz@gmail.com>
2018-01-21 11:26:31 +0100

    hello route

--- meetingserver/urls.py
+++ meetingserver/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from views import hello_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_view.hello)
 ]
