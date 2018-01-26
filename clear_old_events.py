141c0d112a66225724cefbc90ec30ed7bf4dd77f
Piotr Pusz <petropusz@gmail.com>
2018-01-26 17:45:16 +0100

    clearing events that have ended (not more than once per 2 minutes; on request to app and on app run)

---
+++ clear_old_events.py
@@ -0,0 +1,30 @@
+
+from meetingserver.models import Meeting
+import pytz  # żeby mieć "timezone-aware" now
+from django.utils.dateparse import parse_duration
+from datetime import datetime, timedelta 
+
+class PastEventClearer:
+
+    def __init__(self, get_response):
+
+        self.get_response = get_response
+        self.clear_past_events()  # clear also on app start
+        self.last_cleared = datetime.now(pytz.utc) 
+
+    def __call__(self, request):
+        
+        if datetime.now(pytz.utc) - self.last_cleared > timedelta(minutes=2):
+            self.clear_past_events()
+            self.last_cleared = datetime.now(pytz.utc) 
+        
+        response = self.get_response(request)
+        
+        #nothing after view rendering
+        
+        return response
+        
+    def clear_past_events(self):
+        
+        Meeting.objects.filter(end__lte=datetime.now(pytz.utc)).delete()  # usuwa też z innych tabel, bo CASCADE
+                                                                          # po co komu <, zróbmy __lte [...]
--- meetingserver/settings.py
+++ meetingserver/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'clear_old_events.PastEventClearer',
 ]
 
 ROOT_URLCONF = 'meetingserver.urls'
